57e64b825de71fa8fb679b05e967af82833e96fd
leedonggyu11 <jrnono0903@naver.com>
2025-03-24 17:48:44 +0900

    feat : 20250324 class total review / array started

---
+++ src/main/java/ch12_arrays/Array01.java
@@ -0,0 +1,12 @@
+package ch12_arrays;
+
+public class Array01 {
+    public static void main(String[] args) {
+        // 배열 선언 및 초기화
+        int[] arr1 = {1,2,3,4,5};
+        int i = 1;
+        System.out.println(i);
+        System.out.println(arr1);
+
+    }
+}
